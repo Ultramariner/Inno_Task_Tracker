45702f769d8e36e11b5d6e0fcf631f6ee58bd506
Ultramar <artyom.odinochenko@yandex.ru>
2026-04-30 20:36:56 +0300

    Add tasks request

--- ui/src/app/tasks/task-list/task-list.component.ts
+++ ui/src/app/tasks/task-list/task-list.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskService } from '../task.service';
 import { Task } from '../task.model';
+import { TaskItemComponent } from '../task-item/task-item.component';
+import { TaskEditorComponent } from '../task-editor/task-editor.component';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TaskItemComponent, TaskEditorComponent],
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss'],
 })
@@ -14,17 +16,61 @@ export class TaskListComponent implements OnInit {
   tasks: Task[] = [];
   loading = true;
 
+  editorVisible = false;
+  editingTask: Task | null = null;
+
   constructor(private taskService: TaskService) {}
 
   ngOnInit() {
+    this.loadTasks();
+  }
+
+  loadTasks() {
+    this.loading = true;
     this.taskService.getTasks().subscribe({
       next: (tasks) => {
         this.tasks = tasks;
         this.loading = false;
       },
-      error: () => {
-        this.loading = false;
-      },
+      error: () => (this.loading = false),
     });
+  }
+
+  openCreate() {
+    this.editingTask = null;
+    this.editorVisible = true;
+  }
+
+  openEdit(task: Task) {
+    this.editingTask = task;
+    this.editorVisible = true;
+  }
+
+  closeEditor() {
+    this.editorVisible = false;
+  }
+
+  saveTask(data: Partial<Task>) {
+    if (this.editingTask) {
+      this.taskService.updateTask(this.editingTask.id, data).subscribe(() => {
+        this.closeEditor();
+        this.loadTasks();
+      });
+    } else {
+      this.taskService.createTask(data).subscribe(() => {
+        this.closeEditor();
+        this.loadTasks();
+      });
+    }
+  }
+
+  toggleTask(task: Task) {
+    this.taskService
+      .updateTask(task.id, { completed: !task.completed })
+      .subscribe(() => this.loadTasks());
+  }
+
+  removeTask(task: Task) {
+    this.taskService.deleteTask(task.id).subscribe(() => this.loadTasks());
   }
 }
